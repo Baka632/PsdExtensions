e853f8162b7ace4b0ea62a981eadd8ce27359e60
Baka632 <stevemc123456@outlook.com>
2025-11-15 14:08:46 +0800

    调整主题设置。

--- src/PsdExtensions.Wpf/App.xaml.cs
+++ src/PsdExtensions.Wpf/App.xaml.cs
@@ -1,14 +1,19 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
 
-namespace PsdExtensions.Wpf
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace PsdExtensions.Wpf;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+#pragma warning disable WPF0001 // 类型仅用于评估，在将来的更新中可能会被更改或删除。
+        // Win11 => Fluent, other => Aero2
+        ThemeMode = OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22000, 0)
+            ? ThemeMode.System
+            : ThemeMode.None;
+#pragma warning restore WPF000
+    }
 }
